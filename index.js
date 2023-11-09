cd155ca6cb4f8d3d97731d6b0261dc4ec41e5b9f
Andre Cantuaria <146020004+andrecantuaria@users.noreply.github.com>
2023-11-09 13:39:52 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
 'use strict'
 
-<<<<<<<<< Temporary merge branch 1
 const TOKEN = 'dlfkhdsfhfw654654fgdfgMORECODE'
-let adress ='215 - 123 Hellow Bay'
-const TOKEN = 'dlfkhdsfhfw654654fgdfg'
 let adress ='444 - 123 Hellow Bay'
 let postalCode = 'R3A 0H9';
->>>>>>>>> Temporary merge branch 2
+
 
 
 // utility functions
-
-<<<<<<<<< Temporary merge branch 1
-function  selectElement(selector) {
-=========
-function  printOptions(selector) {
->>>>>>>>> Temporary merge branch 2
-    return document.querySelector(selector)
-}
+return document.querySelector(selector)
 
 let businessNAme = 'Super Pizza Place'
 

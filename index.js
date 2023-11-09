c7f6c7531d86a9fb32de61765427423e8e860edb
Andre Cantuaria <andrecantuaria@gmail.com>
2023-11-09 13:18:05 -0600

    modify index

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 'use strict'
 
+<<<<<<< HEAD
 const TOKEN = 'dlfkhdsfhfw654654fgdfgMORECODE'
 let adress ='215 - 123 Hellow Bay'
+=======
+const TOKEN = 'dlfkhdsfhfw654654fgdfg'
+let adress ='444 - 123 Hellow Bay'
+let postalCode = 'R3A 0H9';
+>>>>>>> c32e8addc2e2b5c8312ad89ac6ba8a40e1676118
 
 
 // utility functions
 
+<<<<<<< HEAD
 function  selectElement(selector) {
+=======
+function  printOptions(selector) {
+>>>>>>> c32e8addc2e2b5c8312ad89ac6ba8a40e1676118
     return document.querySelector(selector)
 }
 
